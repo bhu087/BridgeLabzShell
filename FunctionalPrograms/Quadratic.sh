0093c039f342a603eac89ebdeda4cc8715a433e6
bhu087 <bhu087@gmail.com>
2020-01-23 16:11:10 +0530

    [Bhushan] Add. Quadratic program and Refactor. updated utility for Quadratic program

---
+++ FunctionalPrograms/Quadratic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source Utility.sh
+echo "This is quadratic equation program"
+echo "Enter a,b and c"
+read a
+read b
+read c
+QuadraticUtility $a $b $c
--- FunctionalPrograms/Utility.sh
+++ FunctionalPrograms/Utility.sh
@@ -39,3 +39,52 @@ Dist=$(echo "$Value" | awk '{print sqrt($1)}')
 clear
 echo "Distance is $Dist"
 }
+
+#Quadratic equation program
+
+QuadraticUtility(){
+a=$1
+b=$2
+c=$3
+#delta value to find the roots
+(( delta = ($b \* $b ) - ( 4 \* $a \* $c) ))
+#delta is 0 then roots are equal anad real
+if [ $delta -eq 0 ]
+then
+	echo -n "Roots are equal"
+	x=$(echo "$a" "$b" 2 | awk '{print -$2/($3*$1)}')
+	echo " X=$x and Y=$x"
+fi
+#delta is greater then 0 then roots are rael and different
+if [ $delta -gt 0 ]
+then
+	echo -n "Roots are real"
+	x=$(echo "$a" "$b" "$delta" | awk '{print (-$2+sqrt($3))/(2*$1)}')
+	y=$(echo "$a" "$b" "$delta" | awk '{print (-$2-sqrt($3))/(2*$1)}')
+	echo " X=$x and Y=$y"
+fi
+#delta is less then 0 the roots are imaginary
+if [ $delta -lt 0 ]
+then
+	echo -n "Roots are imaginary"
+	rePart=$(echo "$a" "$b" | awk '{print -$2/(2*$1)}')
+	imPart=$(echo "$a" "$delta" | awk '{print sqrt(-$2/(2*$1))}')
+	echo " $rePart + $imPart i and $rePart - $imPart i"
+fi
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
