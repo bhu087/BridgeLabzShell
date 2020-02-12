d32334f22a5278d283ad5657b47d1bde0994754c
bhu087 <bhu087@gmail.com>
2020-02-12 09:33:02 +0530

    [Bhushan] Refactor. Duplicate folders deleted

--- FunctionalPrograms/Distance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-source Utility.sh
-echo "Enter 2 points X and Y"
-read X
-read Y
-#Calling Distance utility
-DistanceUtility $X $Y
--- FunctionalPrograms/Quadratic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-source Utility.sh
-echo "This is quadratic equation program"
-echo "Enter a,b and c"
-read a
-read b
-read c
-QuadraticUtility $a $b $c
